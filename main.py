cdb5ef10aa473feac2019d5d1b93044f266e45cf
anujeshansh <anujesh.ansh.btech2021@sitpune.edu.in>
2022-12-13 02:18:28 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import pyttsx3 # to make it speak
+import PyPDF3
+
+speaker = pyttsx3.init()
+
+#open pdf file
+book = open('oops.pdf','rb') # here we enter the file name
+pdfReader = PyPDF3.PdfFileReader(book)
+
+#view nu. of pages
+pages = pdfReader.numPages # Here it stores the total number of pages of a file
+print("Total Number of Pages:- ", pages)
+
+#reading a particular page
+# page = pdfReader.getPage(7)
+# text = page.extractText()
+
+#reading from the starting & by chaning the 0, it can read from any particular page
+for i in range(6,pages):
+    page = pdfReader.getPage(i)
+    text = page.extractText() # it extracts text from the page and stores in variable 'text'
+    speaker.say(text) # It reads out the complete data saved in variable 'text'
+    speaker.runAndWait() # it speaks and wait for a response
+
+#speaking function
+
+speaker.say("Thank You for Listening")
+speaker.runAndWait()
